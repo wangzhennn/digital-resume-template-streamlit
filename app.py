21342fc261cbdccef75c52569def9762b13931f0
Zhen Wang <101124460+wangzhennn@users.noreply.github.com>
2024-02-11 20:40:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ st.image(ceo_01, width=600)
 st.write("---")
 # --- Project Portfolio 4 ---
 st.write("**A Literature Review of Supervisor Developmental Feedback**")
-st.write("01/2021 - 06/2021 | Bachelor's thesis, Grade:A")
+st.write("01/2021 - 06/2021 | Bachelor's thesis, Grade: A")
 st.write("Based on massive supervisor developmental feedback (SDF) studies published in SSCI/CSSCI journals over the past two decades, this study clarifies the concept and measurements of SDF, categorizes existing research theories and summarizes influential mechanisms. Following these, various future research directions are proposed")
 st.image(sdf_04, width=700)
 st.write("---")
